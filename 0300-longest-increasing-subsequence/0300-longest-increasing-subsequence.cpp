0c463ff392a5d0dd93f8ff7ce699419ff2db0cf9
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2024-01-11 21:55:51 +0530

    Time: 7 ms (80.72%), Space: 10.6 MB (92.28%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,27 +1,23 @@
+// TC = O(N * log N) - N binary searches of O(log N) each
+// SC = O(N)
+
 class Solution {
 public:
-    int solve(int ind, int prev,vector<int>& nums,int n,vector<vector<int>>&dp){
-
-        if(ind == n) return  0 ;
-
-        if(dp[ind][prev+1] != -1){
-            return dp[ind][prev+1];
-        }
-        //not take case
-        int len = 0 + solve(ind+1,prev,nums,n,dp);
-
-        //take case 
-        if(prev == -1 || nums[ind] > nums[prev]){
-            len  = max(len,1 + solve(ind+1,ind,nums,n,dp));
-        }
-
-        return dp[ind][prev+1] = len;
-        
-    }
     int lengthOfLIS(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>>dp(n,vector<int>(n+1,-1));
-        return solve(0,-1,nums,n,dp);
-
+        vector<int> temp;
+        temp.push_back(nums[0]);
+        int l = 1; // len = 1 as we pushed nums[0]
+        // LOGIC : Create a temp array that adds elements if greater than the last element, and if not, overwrites smallest greater element. NOTE : temp does not store the longest increasing subsequence, we are just utilizing the length of temp to get our answer.
+        for(int i=1;i<nums.size();i++){
+            if(temp.back() < nums[i]){ // If nums[i] is greater, just add it at the back
+                temp.push_back(nums[i]);
+                l++;
+            }
+            else{ // If not, just replace the smallest element greater than nums[i] with nums[i]
+                int ind = lower_bound(temp.begin(),temp.end(),nums[i]) - temp.begin(); // inbuilt c++ function to perform binary search O(log N) to search for the third parameter in the array or element just greater than it. Do - temp.begin() to get an integer index value
+                temp[ind] = nums[i];
+            }
+        }
+        return l;
     }
 };
