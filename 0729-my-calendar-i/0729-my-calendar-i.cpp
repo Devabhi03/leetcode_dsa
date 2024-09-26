a745f0d087cc65cfeaf82ec6739ae3dff4b52f62
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2024-09-26 22:45:44 +0530

    Time: 72 ms (79.52%), Space: 42.3 MB (71.5%) - LeetHub

--- 0729-my-calendar-i/0729-my-calendar-i.cpp
+++ 0729-my-calendar-i/0729-my-calendar-i.cpp
@@ -1,23 +1,27 @@
+struct Node{
+    int s, t;
+    Node *left, * right;
+    Node(int s, int t): s(s), t(t), left(NULL), right(NULL){}
+};
 class MyCalendar {
+    Node* root;
 public:
-    unordered_set<int> m; // Stores individual time points
-
-    MyCalendar() {}
-
+    MyCalendar(): root(NULL) {}
+    
+    bool dfs(int s, int t, Node* &curr){
+        if (!curr) {
+            curr=new Node(s, t);
+            return 1;
+        }
+        if (t<=curr->s) return dfs(s, t, curr->left);
+        else if (s>=curr->t) return dfs(s, t, curr->right);
+        else return 0;
+    }
     bool book(int start, int end) {
-        unordered_set<int> subset; // Temporary set to store current interval
-        for (int i = start; i < end; i++) {
-            if (m.count(i) > 0) { // Check if any point is already booked
-                return false;
-            }
-            subset.insert(i);
-        }
-        for (auto x : subset) { // Insert current interval points into main set
-            m.insert(x);
-        }
-        return true;
+        return dfs(start, end, root);
     }
 };
+
 /**
  * Your MyCalendar object will be instantiated and called as such:
  * MyCalendar* obj = new MyCalendar();
