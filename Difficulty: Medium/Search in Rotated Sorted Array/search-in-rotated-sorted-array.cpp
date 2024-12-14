9838fc903a2331dad9f4e7ae250b16b1e3568887
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2024-12-14 23:15:46 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
+++ Difficulty: Medium/Search in Rotated Sorted Array/search-in-rotated-sorted-array.cpp
@@ -4,73 +4,30 @@ using namespace std;
 
 
 // } Driver Code Ends
-
 class Solution {
-  public:
-    int findKey(vector<int> arr, int i, int j, int key){
-        while(i<=j){
-            if(arr[i]==key){
-                return i;
+   public:
+    int search(vector<int>& arr, int target) {
+        // complete the function here
+        
+        int n = arr.size();
+        int lo = 0,hi = n-1;
+        while(lo<=hi){
+            int mid = lo + (hi-lo)/2;
+            
+            if(arr[mid]==target) return mid;
+            if(arr[lo]<=arr[mid]){  // Left half is sorted
+                if(target >= arr[lo] && target < arr[mid]) hi = mid-1;  // Target lies in the left half
+                else lo = mid+1; // Target lies in the right half
             }
-            else if(arr[j] == key){
-                return j;
-            }
-            int mid = (i+j)/2;
-            if(arr[mid]==key){
-                return mid;
-            }
-            else if(arr[mid] > key){
-                j = mid-1;
-            }
-            else{
-                i = mid+1;
+            
+            else { // Right half is sorted
+                if(target <= arr[hi] && target > arr[mid]) lo = mid+1; // Target lies in the right half
+                else hi = mid-1;  // Target lies in the left half
             }
         }
         return -1;
     }
-    int search(vector<int>& arr, int key) {
-        // complete the function here
-        int n = arr.size();
-        int ans = -1;
-        int l = 0, r = n-1, point = 0;
-        if(arr[0]==key){
-            return 0;
-        }
-        if(arr[n-1]==key){
-            return n-1;
-        }
-        if(arr[l] < arr[r]){
-            return findKey(arr, 0, n-1, key);
-        }
-        while(l < r){
-            int mid = (l+r)/2;
-            if(mid>0 && arr[mid]<arr[mid-1]){
-                point = mid;
-                break;
-            }
-            else if(mid<n && arr[mid]>arr[mid+1]){
-                point = mid+1;
-                break;
-            }
-            else{
-                if(arr[mid+1] < arr[l]){
-                    r = mid-1;
-                }
-                else
-                    l = mid+1;
-            }
-        }
-        
-        if(arr[n-1] > key){
-            ans = findKey(arr, point, n-1, key);
-        }
-        else{
-            ans = findKey(arr, 0, point-1, key);
-        }
-        return ans;
-    }
 };
-
 
 //{ Driver Code Starts.
 int main() {
@@ -90,6 +47,7 @@ int main() {
         cin >> key;
         Solution ob;
         cout << ob.search(arr, key) << endl;
+        cout << "~" << endl;
     }
     return 0;
 }
