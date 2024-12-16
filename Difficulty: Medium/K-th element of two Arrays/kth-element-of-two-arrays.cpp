0c35c7868e81f81cd0d28a458c34c40b17bf5ee9
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2024-12-16 23:37:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
+++ Difficulty: Medium/K-th element of two Arrays/kth-element-of-two-arrays.cpp
@@ -6,39 +6,13 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
-    int kthElement(int k, vector<int>& arr1, vector<int>& arr2) {
+    int kthElement(vector<int>& arr, vector<int>& brr, int k) {
         // code here
-        int n = arr1.size();
-        int m = arr2.size();
+        vector<int> temp(arr.begin(), arr.end());
+        temp.insert(temp.end(), brr.begin(), brr.end());
+        sort(temp.begin(), temp.end());
+        return temp[k-1];
         
-        if(k==n+m){
-            return max(arr1[n-1],arr2[m-1]);
-        }
-        if(k==1){
-            return min(arr1[0],arr2[0]);
-        }
-        if(n>m){
-            return kthElement(k,arr2,arr1);
-        }
-        int low = max(0,k-m), high = min(k,n);
-        while(low<=high){
-            int mid = (low+high)>>1;
-            int idx = k-mid;
-            
-            int arr1Left = (mid == 0) ? INT_MIN : arr1[mid - 1];
-            int arr1Right = (mid == n) ? INT_MAX : arr1[mid];
-            int arr2Left = (idx == 0) ? INT_MIN : arr2[idx - 1];
-            int arr2Right = (idx == m) ? INT_MAX : arr2[idx];
-    
-            if (arr1Left <= arr2Right && arr2Left <= arr1Right) {
-                return max(arr1Left, arr2Left);
-            } else if (arr1Left > arr2Right) {
-                high = mid - 1;
-            } else {
-                low = mid + 1;
-            }
-             
-        }
     }
 };
 
@@ -55,22 +29,22 @@ int main() {
         cin.ignore();
         string input;
         int num;
-        vector<int> arr1, arr2;
+        vector<int> a, b;
 
         getline(cin, input);
         stringstream s2(input);
         while (s2 >> num) {
-            arr1.push_back(num);
+            a.push_back(num);
         }
 
         getline(cin, input);
         stringstream s3(input);
         while (s3 >> num) {
-            arr2.push_back(num);
+            b.push_back(num);
         }
 
         Solution ob;
-        cout << ob.kthElement(k, arr1, arr2) << endl;
+        cout << ob.kthElement(a, b, k) << endl << "~\n";
     }
     return 0;
 }
