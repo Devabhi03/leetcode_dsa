01f7ee82b47233fdf2737f76c555ceb71db5955c
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-01-30 22:07:33 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
+++ Difficulty: Hard/N-Queen Problem/nqueen-problem.cpp
@@ -4,71 +4,104 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+
 // } Driver Code Ends
 // User function Template for C++
 
-class Solution{
-public:
-    vector<vector<int>> res;
-    bool check( int x, int y, int n, vector<vector<int>> &chessBoard ){
-        int i, j;
-        i = x-1; j = y;
-        while ( i >= 0 ) if ( chessBoard[i--][j] == 1 ) return false;
-        i = x+1; j = y;
-        while ( i < n ) if ( chessBoard[i++][j] == 1 ) return false;
-        i = x; j = y-1;
-        while ( j >= 0 ) if ( chessBoard[i][j--] == 1 ) return false;
-        i = x-1; j = y-1;
-        while ( i >= 0 && j >= 0 ) if ( chessBoard[i--][j--] == 1 ) return false;
-        i = x+1; j = y-1;
-        while ( i < n && j >= 0 ) if ( chessBoard[i++][j--] == 1 ) return false;
-        return true;
-    }
-    void rec( int j, int n, vector<int> &pos, vector<vector<int>> &chessBoard ){
-        if ( j == n ){
-            res.push_back(pos);
+class Solution {
+  public:
+     void solve(int row,int n, vector<int> temp, vector<vector<int>> &res,vector<int> &upCol,vector<int> &rightDiag,vector<int> &leftDiag){
+        if(row==n){
+            res.push_back(temp);
             return;
         }
-        for ( int i = 0; i<n; i++ ){
-            if ( check(i, j, n, chessBoard) ) {
-                pos.push_back(i+1);
-                chessBoard[i][j] = 1;
-                rec(j+1, n, pos, chessBoard);
-                chessBoard[i][j] = 0;
-                pos.pop_back();
+        
+        for(int i=0;i<n;i++){
+            if(upCol[i]==0 && leftDiag[n-1+row-i]==0 && rightDiag[row+i]==0){
+                upCol[i]=1;
+                leftDiag[n-1+row-i]=1 ;
+                rightDiag[row+i]=1;
+                temp.push_back(i+1);
+                solve(row+1,n,temp,res,upCol,rightDiag,leftDiag);
+                upCol[i]=0;
+                leftDiag[n-1+row-i]=0 ;
+                rightDiag[row+i]=0;
+                  temp.pop_back();
             }
         }
+        
+        
     }
     vector<vector<int>> nQueen(int n) {
-        vector<vector<int>> chessBoard(n+1, vector<int>(n+1, 0));
-        vector<int> pos; rec(0, n, pos, chessBoard);
-        return res;
+        vector<vector<int>> res;
+        vector<int> temp;
+        
+         vector<int> upCol(n, 0), rightDiag(2 * n - 1, 0), leftDiag(2 * n - 1, 0);
+        
+        solve(0,n,temp,res,upCol,rightDiag,leftDiag);
+         
+         return res;
+        
+    }
+
+bool check(int row,int col,vector<vector<int>> grid){
+        int n= grid.size();
+        //check col
+        for(int i=0;i<row;i++){
+            if(grid[i][col]==1)
+            return false;
+        }
+        
+        //check left diagonally
+        int r=row-1,c=col-1;
+        while(r>=0 && c>=0){
+            if(grid[r][c]==1)
+            return false;
+            
+            r--;
+            c--;
+        }
+        
+        //right diagonally
+         r=row-1,c=col+1;
+        while(r>=0 && c<n){
+            if(grid[r][c]==1)
+            return false;
+            
+            r--;
+            c++;
+        }
+        
+        return true;
     }
 };
 
-
 //{ Driver Code Starts.
 
-int main(){
+int main() {
     int t;
-    cin>>t;
-    while(t--){
+    cin >> t;
+    while (t--) {
         int n;
-        cin>>n;
-        
+        cin >> n;
+
         Solution ob;
         vector<vector<int>> ans = ob.nQueen(n);
-        if(ans.size() == 0)
-            cout<<-1<<"\n";
+        if (ans.size() == 0)
+            cout << -1 << "\n";
         else {
-            for(int i = 0;i < ans.size();i++){
-                cout<<"[";
-                for(int u: ans[i])
-                    cout<<u<<" ";
-                cout<<"] ";
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
             }
-            cout<<endl;
+            cout << endl;
         }
+
+        cout << "~"
+             << "\n";
     }
     return 0;
 }
