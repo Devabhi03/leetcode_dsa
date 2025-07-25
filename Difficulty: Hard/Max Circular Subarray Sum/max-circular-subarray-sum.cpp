d381217b2ad7ab6b32d2d04bcf39863ac5363784
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-07-25 22:26:05 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
+++ Difficulty: Hard/Max Circular Subarray Sum/max-circular-subarray-sum.cpp
@@ -1,59 +1,23 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
   public:
-    // arr: input array
-    // Function to find maximum circular subarray sum.
-      int circularSubarraySum(vector<int> &arr) {
-        // your code here
-        int n=arr.size();
-        int maxi1=0,sum1=0,total=0;
-        for(int i=0;i<n;i++){
-            sum1+=arr[i];
-            total+=arr[i];
-            if(sum1>maxi1) maxi1=max(sum1,maxi1);
-            if(sum1<0) sum1=0;
+    int maxCircularSum(vector<int> &arr) {
+        int totalSum = 0;
+        int currMaxSum = 0, currMinSum = 0;
+        int maxSum = arr[0], minSum = arr[0];
+
+        for (int i = 0; i < arr.size(); i++) {
+            currMaxSum = max(currMaxSum + arr[i], arr[i]);
+            maxSum = max(maxSum, currMaxSum);
+            currMinSum = min(currMinSum + arr[i], arr[i]);
+            minSum = min(minSum, currMinSum);
+            totalSum = totalSum + arr[i];
         }
-        
-        int maxi2=0,sum2=0;
-        for(int i=0;i<n;i++){
-            sum2+=arr[i];
-            maxi2=min(maxi2,sum2);
-            if(sum2>0) sum2=0;
-        }
-        
-        return max(maxi1,total-maxi2);
+
+        int normalSum = maxSum;
+        int circularSum = totalSum - minSum;
+        if (minSum == totalSum)
+            return normalSum;
+
+        return max(normalSum, circularSum);
     }
 };
-
-//{ Driver Code Starts.
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        vector<int> arr;
-        string input;
-
-        // Read first array
-        getline(cin, input);
-        stringstream ss(input);
-        int number;
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-
-        Solution ob;
-        int res = ob.circularSubarraySum(arr);
-
-        cout << res << endl;
-    }
-    return 0;
-}
-
-// } Driver Code Ends
