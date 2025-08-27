0c1dc5aa830b485ec6a4b51cf2e7e5819f6ae989
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-08-27 23:43:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
+++ Difficulty: Medium/Count the number of possible triangles/count-the-number-of-possible-triangles.cpp
@@ -1,59 +1,21 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
 class Solution {
-   public:
-    // Function to count the number of possible triangles.
+  public:
     int countTriangles(vector<int>& arr) {
-        // code here
-        int n=arr.size();
-        sort(arr.begin(),arr.end());
-        int c=0; //count
-        for(int high=n-1;high>=2;high--)
-        {
-            int low=0,i=high-1;
-            while(low<i)
-            {
-                if(arr[low]+arr[i]>arr[high])
-                {
-                    c+=i-low;
-                    i--;
-                }
-                else
-                    low++;
+    int n = arr.size();
+    sort(arr.begin(), arr.end());
+    int count = 0;
+
+    for (int k = n - 1; k >= 2; k--) {
+        int i = 0, j = k - 1;
+        while (i < j) {
+            if (arr[i] + arr[j] > arr[k]) {
+                count += (j - i); // all elements between i and j are valid
+                j--;
+            } else {
+                i++;
             }
         }
-        return c;
+    }
+    return count;
     }
 };
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore(); // To ignore the newline after the integer input
-    while (t--) {
-        int n;
-        vector<int> a;
-        string input;
-
-        // Input format: first number n followed by the array elements
-        getline(cin, input);
-        stringstream ss(input);
-        int num;
-        while (ss >> num)
-            a.push_back(num);
-
-        Solution obj;
-        cout << obj.countTriangles(a) << "\n~\n";
-    }
-
-    return 0;
-}
-
-// } Driver Code Ends
