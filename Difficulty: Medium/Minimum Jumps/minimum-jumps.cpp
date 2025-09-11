2cf3893a350ffe9cb21cda352be0ff18e342da65
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-09-11 23:40:11 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
+++ Difficulty: Medium/Minimum Jumps/minimum-jumps.cpp
@@ -1,62 +1,26 @@
-//{ Driver Code Starts
-#include <bits/stdc++.h>
-using namespace std;
-
-
-// } Driver Code Ends
-
 class Solution {
-   public:
-    
-    int dp[10005];
-    int solve(int ind , vector<int> &arr){
-        if(ind == arr.size()-1) return 0;
-        if(ind >=arr.size()) return 1e7;
-        
-        if(dp[ind] != -1) return dp[ind];
-        
-        int ans = 1e7;
-        
-        for(int i = 1 ; i <= arr[ind] ; i++){
-            ans = min(ans , 1 + solve(ind + i , arr));
-        }
-        return dp[ind] = ans;
-    }
+  public:
     int minJumps(vector<int>& arr) {
-        // code here
-        memset(dp ,-1, sizeof(dp));
-        
-        int ans = solve(0 , arr);
-        if(ans == 1e7) return -1;
-        return ans;
-        
-        
+        int n = arr.size();
+        if (n <= 1) return 0;          // Already at last index
+        if (arr[0] == 0) return -1;    // Can't move anywhere
+
+        int maxReach = arr[0];  // Farthest index reachable
+        int steps = arr[0];     // Steps left in current jump
+        int jumps = 1;          // We make the first jump from index 0
+
+        for (int i = 1; i < n; i++) {
+            if (i == n - 1) return jumps;  // Reached last index
+
+            maxReach = max(maxReach, i + arr[i]);
+            steps--;
+
+            if (steps == 0) {  // Need to make another jump
+                jumps++;
+                if (i >= maxReach) return -1;  // Can't move further
+                steps = maxReach - i;
+            }
+        }
+        return -1; // If end is never reached
     }
 };
-
-
-
-//{ Driver Code Starts.
-
-int main() {
-    int t;
-    cin >> t;
-    cin.ignore();
-    while (t--) {
-        int n, i, j;
-        vector<int> arr;
-        string ip;
-        int number;
-        getline(cin, ip);
-        stringstream ss(ip);
-
-        while (ss >> number) {
-            arr.push_back(number);
-        }
-        Solution obj;
-        cout << obj.minJumps(arr) << endl << "~\n";
-    }
-    return 0;
-}
-
-// } Driver Code Ends
