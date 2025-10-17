fecc9bcbbbb32869005110ccad4e285c3d480e49
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-10-17 23:04:55 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/BST to greater sum tree/bst-to-greater-sum-tree.cpp
@@ -0,0 +1,29 @@
+/*
+class Node {
+  public:
+    int data;
+    Node* left;
+    Node* right;
+
+    Node(int value) {
+        data = value;
+        left = nullptr;
+        right = nullptr;
+    }
+};*/
+
+class Solution {
+   public:
+    int sum = 0;
+    void transformTree(Node *root) {
+        // code here
+        if(!root){
+            return;
+        }
+        transformTree(root->right);
+        int rootData = root->data;
+        root->data = sum;
+        sum += rootData;
+        transformTree(root->left);
+    }
+};
