3d96c8b2aad53cbfdba10f4ebeac3fe468ae7cd9
Devabhi03 <111748301+Devabhi03@users.noreply.github.com>
2025-12-20 15:40:40 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search insert position of K in a sorted array/search-insert-position-of-k-in-a-sorted-array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+  public:
+    int searchInsertK(vector<int> &arr, int k) {
+    // code here
+    int n = arr.size(); 
+    if(k <= arr[0]) return 0;
+    if(k > arr[n-1]) return n;
+    
+    int l = 0, r = n-1;
+    while(l < r) {
+        int m =  l + (r - l + 1)/2;
+        //  last T in T*f*
+        if(arr[m] < k)
+            l = m;
+        else
+            r = m - 1;
+    }
+    return l+1;
+}
+};
